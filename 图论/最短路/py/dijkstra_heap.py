794ecb1c7ee861575b551153aeec0dcc5a0d2671
Pluto Evans <deep_coder@hotmail.com>
2024-05-04 00:21:48 +0800

    2024-05-04

--- 图论/最短路/py/dijkstra_heap.py
+++ 图论/最短路/py/dijkstra_heap.py
@@ -1,15 +1,17 @@
 def dijkstra(edges: List[List[int]], n: int, src: int) -> int:
-    g = defaultdict(dict)
+    g = defaultdict(lambda: defaultdict(lambda: inf))
+    # 有向图情况, 注意处理重边！
     for u, v, w in edges:
         g[u][v] = w
-    hpq = [[0, src]]
     dist = [-1] * n
-    while hpq:
-        d, u = heapq.heappop(hpq)
-        if dist[u] != -1: continue
-        dist[u] = d
+    dist[src] = 0
+    h = [[0, src]]
+    while h:
+        d, u = heappop(h)
+        if d > dist[u]: continue
         for v, w in g[u].items():
-            heapq.heappush(hpq, [d + w , v])
-            
-    if min(dist) == -1: return -1
-    else: return max(dist)
+            nd = d + w
+            if dist[v] == -1 or dist[v] > nd:
+                dist[v] = nd
+                heappush(h, [nd, v])
+    return dist
