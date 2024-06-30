05edd19955ed1fc9130164a0ce7345dff6def6a5
Evan Cao <deep_coder@hotmail.com>
2024-06-30 20:28:27 +0800

    fix:更新质因数分解模板

--- 算法/数学/质因数分解.py
+++ 算法/数学/质因数分解.py
@@ -1,6 +1,6 @@
 def fac(N):
     res = []
-    for i in range(2, int(sqrt(N)) + 1):
+    for i in range(2, isqrt(N) + 1):
         if N % i == 0: 
             while N % i == 0:
                 N //= i
